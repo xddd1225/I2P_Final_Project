ab2c87de0e308d54272a7cc840250120750776ce
Jc Hu <hujosh0509@gmail.com>
2025-06-12 00:55:31 +0800

    update some button image

--- Scene/StageSelectScene.cpp
+++ Scene/StageSelectScene.cpp
@@ -15,19 +15,19 @@ void StageSelectScene::Initialize() {
     AddNewObject(new Engine::Label("Select Map", "PixelatedElegance.ttf", 72, halfW, halfH / 4, 255, 255, 255, 255, 0.5, 0.5));
 
     // Stage 1 button
-    auto* btn1 = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH - 100, 400, 100);
+    auto* btn1 = new Engine::ImageButton("stage-select/button.png", "stage-select/buttonOn.png", halfW - 200, halfH - 100, 400, 100);
     btn1->SetOnClickCallback(std::bind(&StageSelectScene::StageOnClick, this, 1));
     AddNewControlObject(btn1);
     AddNewObject(new Engine::Label("Map 1", "PixelatedElegance.ttf", 48, halfW, halfH - 45, 0, 0, 0, 255, 0.5, 0.5));
 
     // Stage 2 button
-    auto* btn2 = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH + 55, 400, 100);
+    auto* btn2 = new Engine::ImageButton("stage-select/button.png", "stage-select/buttonOn.png", halfW - 200, halfH + 55, 400, 100);
     btn2->SetOnClickCallback(std::bind(&StageSelectScene::StageOnClick, this, 2));
     AddNewControlObject(btn2);
     AddNewObject(new Engine::Label("Random", "PixelatedElegance.ttf", 48, halfW, halfH + 110, 0, 0, 0, 255, 0.5, 0.5));
 
     // Back button
-    auto* backBtn = new Engine::ImageButton("stage-select/dirt.png", "stage-select/floor.png", halfW - 200, halfH + 200, 400, 100);
+    auto* backBtn = new Engine::ImageButton("stage-select/button.png", "stage-select/buttonOn.png", halfW - 200, halfH + 200, 400, 100);
     backBtn->SetOnClickCallback(std::bind(&StageSelectScene::BackOnClick, this));
     AddNewControlObject(backBtn);
     AddNewObject(new Engine::Label("Back", "PixelatedElegance.ttf", 48, halfW, halfH + 260, 0, 0, 0, 255, 0.5, 0.5));
